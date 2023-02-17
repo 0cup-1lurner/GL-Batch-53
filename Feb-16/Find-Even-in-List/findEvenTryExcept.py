4ee7190648ac9c3175ac0ca26ecffbb4006cae82
0cup <neelalohitha.k@geeklurn.com>
2023-02-17 16:17:08 +0530

    add findEvenTryExcept.py

--- Feb-16/Find-Even-in-List/findEvenTryExcept.py
+++ Feb-16/Find-Even-in-List/findEvenTryExcept.py
@@ -7,9 +7,10 @@ def find_even_2(l):
     i = 0
     try:
         while l[i] % 2 != 0:
-            i += 1
+            i += 1  # Continue to check the next element in the list if l[i] is odd
     except IndexError:
+	# List index out of range as i >= len(l) if no even number is present
         raise ValueError("No even number found")
     else:
-        #  Return the even number
+        # Return the first even number
         return l[i]
