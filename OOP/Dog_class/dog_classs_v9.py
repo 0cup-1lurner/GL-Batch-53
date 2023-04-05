67aa666a7774858182716952262d3b5d92fb37eb
Empty-Cup <neelalohitha.k@geeklurn.com>
2023-04-05 22:00:51 +0530

    Add v9 of Dog class: Modify  method in Parent class

---
+++ OOP/Dog_class/dog_classs_v9.py
@@ -0,0 +1,52 @@
+
+# Parent class
+class Dog:
+    """ A python class to represent Dog"""
+    # Class Attribute
+    species = "Canis familiaris"
+
+    def __init__(self, name, age):
+        """ param:
+            name: str
+            age: int
+
+            Creates an instance of the Dog class
+"""
+        self.name = name
+        self.age = age
+
+
+    def __str__(self):
+        """ Give info about the instance"""
+        return f"Dog: {self.name} is {self.age} years old."
+
+    def __repr__(self):
+        return f"Dog({repr(self.name)},{repr(self.age)})"
+
+    def speak(self, sound):
+        """ sound: str
+
+            returns a str that represents how a dog barks
+        """
+        return f'{self.name} barks: {sound}'
+
+# Child Class
+class JackRusselTerrier(Dog):
+
+    def __str__(self):
+        """ Give info about the instance"""
+        return f"A JackRusselTerrier: {self.name} is {self.age} years old."
+    
+    def __repr__(self):
+        return f"JackRusselTerrier({repr(self.name)},{repr(self.age)})" 
+
+    def speak(self, sound = "Arf"):
+        """ sound: str
+
+            returns a str that represents how a dog barks
+        """
+        return f'{self.name} says {sound}'
+
+# Child Class
+class Bulldog(Dog):
+    pass
